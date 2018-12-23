5127d1ad57e7854dd879884a4fb3410b8cfedee0
Pangkevin <kevin94310@hotmail.fr>
2018-12-23 22:57:12 +0100

    J-biblio-MavenTravisConfiguration : Maven project created and travis implementation

---
+++ src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/App.java
@@ -0,0 +1,13 @@
+package io.github.oliviercailloux.y2018.jbiblio.j_biblio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

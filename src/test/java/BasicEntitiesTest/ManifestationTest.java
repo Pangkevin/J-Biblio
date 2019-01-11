f09d5352feb77820b6e51351e83053b128410e0d
Vima Haddad <vima.haddad@gmail.com>
2019-01-11 23:31:18 +0100

    ManifestationItemEntities: Add BasicEntities + BasicEntitiesTest

---
+++ src/test/java/BasicEntitiesTest/ManifestationTest.java
@@ -0,0 +1,30 @@
+package BasicEntitiesTest;
+
+import io.github.oliviercailloux.y2018.jbiblio.j_biblio.BasicEntities.Manifestation;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+
+public class ManifestationTest {
+	@Test
+	void createManifestation() {
+		// Testing Manifestation Constructor
+		Collection<Integer> idItems = new ArrayList<>();
+		idItems.add(34);
+		idItems.add(77);
+
+		Collection<String> titleOfTheManifestation = new ArrayList<>();
+		titleOfTheManifestation.add("Harry Potter");
+
+		Manifestation manifestation = new Manifestation(512, idItems, titleOfTheManifestation);
+
+		assertEquals(manifestation.getIdManifestation(), 512);
+		assertThat(manifestation.getIdItems(), contains(34, 77));
+		assertThat(manifestation.getTitleOfTheManifestation(), contains("Harry Potter"));
+
+	}
+}

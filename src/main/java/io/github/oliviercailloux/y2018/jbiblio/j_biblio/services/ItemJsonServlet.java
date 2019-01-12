8e204c2933228e482fa595344ab9c87f20c7d386
Juan175 <juan.marco17@hotmail.com>
2019-01-13 00:55:36 +0100

    ImplementJSONFormat : Modification in ItemJsonServlet and ItemJsonServletTest

--- src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/ItemJsonServlet.java
+++ src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/ItemJsonServlet.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.logging.Logger;
-
+import java.util.ArrayList;
+import java.util.Collection;
 import javax.json.stream.JsonGenerationException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,77 +22,83 @@ import io.github.oliviercailloux.y2018.jbiblio.j_biblio.basicEntities.Item;
 public class ItemJsonServlet extends HttpServlet {
 	private static final Logger LOGGER = Logger.getLogger(ItemJsonServlet.class.getCanonicalName());
 
+	// simulates the database
+	static final Collection<Item> itemsList = new ArrayList<Item>() {
+		{
+			add(new Item(412, "A12S3", "DS21T47DT"));
+			add(new Item(413, "A12S3", "DS21T47DT"));
+			add(new Item(414, "A12S3", "DS21T47DT"));
+		}
+	};
+	@SuppressWarnings("static-access")
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
 		resp.setContentType("application/json");
 		resp.setLocale(Locale.ENGLISH);
-		try {
+		
 			/**
 			 * This mapper provides functionality for converting between Java objects and
 			 * matching JSON constructs
 			 */
 			ObjectMapper mapper = new ObjectMapper();
-			Item item = new Item(412, "A12S3", "DS21T47DT");
-			/**
-			 * transform a object item to a Json format in string format
-			 */
-			String jsonItem = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(item);
-			/**
-			 * Page successfully processed
-			 */
+			try {
+			//	Parse int 
+				String idItem = req.getParameter("idItem");
+				int idItemInt = Integer.parseInt(idItem);
+				
+				// Search object in collection
+				for (Item item : this.itemsList) {
 
+					if (item.getIdItem() == idItemInt) {
+						// transform a object item to a Json format in string format
+						String jsonItem = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(item);
+						// Page successfully processed
+						resp.setStatus(200);
+						// Send message to customer
+						resp.getWriter().println(jsonItem);
+						// display logs in console
+						LOGGER.info(" Display object in JSON format " + jsonItem);
+						// If found, we leave the loop
+						break;
+
+					}
+
+				}
+
+			} catch (JsonGenerationException | NumberFormatException e) {
+				// Send error status
+
+				LOGGER.warning(" Error  " + e.toString());
+				resp.setStatus(404);
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "The research failed");
+
+			}
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException
+	{
+		String idItem = req.getParameter("412");
+		String itemIdentifier = req.getParameter("A12S3");
+		String fingerprint = req.getParameter("DS21T47DT");
+
+		try {
+			Item item = new Item(Integer.parseInt(idItem), itemIdentifier, fingerprint);
+			LOGGER.info("Item idItem : " + item.getIdItem());
+			LOGGER.info("Item itemIdentifier : " + item.getItemIdentifier());
+			LOGGER.info("Item fingerprint : " + item.getFingerprint());
+			
 			resp.setStatus(200);
-			/**
-			 * 
-			 * Send message to custumer
-			 */
-			resp.getWriter().println(jsonItem);
-			/**
-			 * display logs in console
-			 */
-
-			LOGGER.info(" Display object in JSON format " + jsonItem);
-
-		} catch (JsonGenerationException e) {
-			/**
-			 * Send error status
-			 */
-			LOGGER.warning(" Display object in JSON format " + e.toString());
-
-			resp.setStatus(404);
+			resp.getWriter().println("The object is successfully insert");
 		}
 
-}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idItem = request.getParameter("412");
-        String itemIdentifier = request.getParameter("A12S3");
-        String fingerprint = request.getParameter("DS21T47DT");
-        
-
-
-
-        try {
-        	Item item = new Item(Integer.parseInt(idItem), itemIdentifier, fingerprint);
-        	LOGGER.info("Item idItem : " + idItem);
-        	LOGGER.info("Item itemIdentifier : " + itemIdentifier);
-        	LOGGER.info("Item fingerprint : " + fingerprint);
-        	
-			response.setStatus(200);
-			response.getWriter().println("The object is successfully insert");
-        }
-        
-        catch (NumberFormatException e) {
+		catch (NumberFormatException e) {
 
 			LOGGER.warning("The insert failed");
-			response.setStatus(400);
-			response.getWriter().println("The insert failed");
-			
-      
-    }
+			resp.setStatus(400);
+			resp.getWriter().println("The insert failed");
 
+		}
 
 	}
-	}
+}
--- src/test/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/ItemJsonServletTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/ItemJsonServletTest.java
@@ -16,6 +16,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.oliviercailloux.y2018.jbiblio.j_biblio.basicEntities.Item;
 
+/**
+ * This class use Mockito to test doPost and doGet methods which are in the
+ * ItemJsonServlet class
+ *
+ */
 class ItemJsonServletTest extends Mockito {
 
 	@Test
@@ -32,19 +37,20 @@ class ItemJsonServletTest extends Mockito {
 			Item item = new Item(412, "A12S3", "DS21T47DT");
 			ObjectMapper mapper = new ObjectMapper();
 			String jsonItem = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(item);
-			/*
-			 * */
 			StringWriter stringWriter = new StringWriter();
 			PrintWriter writer = new PrintWriter(stringWriter);
 			/*
 			 * mock the behavior of response to return the value of writer
 			 */
+			when(request.getParameter("idItem")).thenReturn("412");
+			when(request.getParameter("itemIdentifier")).thenReturn("A12S3");
+			when(request.getParameter("fingerprint")).thenReturn("DS21T47DT");
 			when(response.getWriter()).thenReturn(writer);
 
 			new ItemJsonServlet().doGet(request, response);
 			writer.flush();
 			/*
-			 * Last step to test if the output of JSON format is correct
+			 * Last step is to test if the output of JSON format is correct
 			 */
 			assertTrue(stringWriter.toString().contains(jsonItem));
 		} catch (IOException e) {
@@ -54,4 +60,37 @@ class ItemJsonServletTest extends Mockito {
 		}
 	}
 
+	@Test
+	public void testDoPost() throws Exception {
+		try {
+			/*
+			 * Create the mock object of HttpServletRequest and HttpServletResponse
+			 */
+			HttpServletRequest request = mock(HttpServletRequest.class);
+			HttpServletResponse response = mock(HttpServletResponse.class);
+
+			StringWriter stringWriter = new StringWriter();
+			PrintWriter writer = new PrintWriter(stringWriter);
+
+			/*
+			 * mock the behavior of response to return the value of writer
+			 */
+			when(request.getParameter("idItem")).thenReturn("412");
+			when(request.getParameter("itemIdentifier")).thenReturn("A12S3");
+			when(request.getParameter("fingerprint")).thenReturn("DS21T47DT");
+
+			when(response.getWriter()).thenReturn(writer);
+
+			new ItemJsonServlet().doPost(request, response);
+			writer.flush();
+			/*
+			 * Last step to test if the output of message error is correct
+			 */
+			assertTrue(stringWriter.toString().contains("The object is successfully insert"));
+		} catch (IOException e) {
+			e.getStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+	}
 }

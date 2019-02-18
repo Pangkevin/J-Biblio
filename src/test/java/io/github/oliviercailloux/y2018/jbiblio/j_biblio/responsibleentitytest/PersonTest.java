21d2b27a6377013f0338343a8f7ca4cf783ed932
Pangkevin <kevin94310@hotmail.fr>
2019-02-18 21:49:15 +0100

    PrCreateResponsibleEntity-Solving : Correct PersonnTest

--- src/test/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/responsibleentitytest/PersonTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/responsibleentitytest/PersonTest.java
@@ -10,12 +10,11 @@ import org.junit.jupiter.api.Test;
 
 import io.github.oliviercailloux.y2018.jbiblio.j_biblio.commonstructures.TimeStampedDescription;
 import io.github.oliviercailloux.y2018.jbiblio.j_biblio.responsibleentity.Person;
-import io.github.oliviercailloux.y2018.jbiblio.j_biblio.responsibleentity.ResponsibleEntity;
 
 class PersonTest {
 
 	@Test
-	void createPersonn() {
+	void createPersonn() throws ParseException {
 		/**
 		 * Init date
 		 */
@@ -24,34 +23,27 @@ class PersonTest {
 		String birthdate = "1565-04-26";
 		String deathdate = "1616-04-23";
 
-		try {
+		// Init Instant Date
+		Instant dateBirth = formatter.parse(birthdate).toInstant();
+		Instant dateDeath = formatter.parse(deathdate).toInstant();
 
-			// Init Instant Date
-			Instant dateBirth = formatter.parse(birthdate).toInstant();
-			Instant dateDeath = formatter.parse(deathdate).toInstant();
+		// Init DateStructure
+		TimeStampedDescription dateStructureBirth = new TimeStampedDescription("BirthDate", dateBirth);
+		TimeStampedDescription dateStructureDeath = new TimeStampedDescription("DeathDate", dateDeath);
 
-			// Init DateStructure
-			TimeStampedDescription dateStructureBirth = new TimeStampedDescription("BirthDate", dateBirth);
-			TimeStampedDescription dateStructureDeath = new TimeStampedDescription("DeathDate", dateDeath);
+		Person person = new Person("Shakespeare, William, 1564-1616", "William", "Shakespeare", dateStructureBirth);
 
-			Person person = new Person("Shakespeare, William, 1564-1616", "William", "Shakespeare", dateStructureBirth);
+		person.setDeathDate(dateStructureDeath);
 
-			person.setDeathDate(dateStructureDeath);
-
-			// test String attributs
-			assertEquals(person.getNameAuthority(), "Shakespeare, William, 1564-1616");
-			assertEquals(person.getFirstName(), "William");
-			assertEquals(person.getLastName(), "Shakespeare");
-			// Test instant date
-			assertEquals(person.getBirthDate().getDate().toString(), "1565-05-05T23:00:00Z");
-			assertEquals(person.getDeathDate().get().getDate().toString(), "1616-04-22T23:00:00Z");
-			assertEquals(person.getMiddleName(), "");
-			assertEquals(person.getMiddleName(), "");
-		} catch (ParseException e) {
-
-			e.printStackTrace();
-			assertFalse(true);
-		}
+		// test String attributs
+		assertEquals(person.getNameAuthority(), "Shakespeare, William, 1564-1616");
+		assertEquals(person.getFirstName(), "William");
+		assertEquals(person.getLastName(), "Shakespeare");
+		// Test instant date
+		assertEquals(person.getBirthDate().getDate().toString(), "1565-05-05T23:00:00Z");
+		assertEquals(person.getDeathDate().get().getDate().toString(), "1616-04-22T23:00:00Z");
+		assertEquals(person.getMiddleName(), "");
+		assertEquals(person.getMiddleName(), "");
 
 	}
 
@@ -69,33 +61,36 @@ class PersonTest {
 
 	/**
 	 * Test Optional attribut deathDate
+	 * 
+	 * @throws ParseException
 	 */
 	@Test
-	void testOptionalAttribut() {
+	void testOptionalAttribut() throws ParseException {
 
-		try {
+		/**
+		 * Init date
+		 */
 
-			Person person = new Person("Shakespeare, William, 1564-1616", "William", "Shakespeare", null);
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String birthdate = "1565-04-26";
+		String deathdate = "1616-04-23";
 
-			assertFalse(person.getDeathDate().isPresent());
+		// Init Instant Date
+		Instant dateBirth = formatter.parse(birthdate).toInstant();
+		Instant dateDeath = formatter.parse(deathdate).toInstant();
 
-			/**
-			 * Init death date
-			 */
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-			String deathdate = "1616-04-23";
-			Instant dateDeath = formatter.parse(deathdate).toInstant();
-			TimeStampedDescription dateStructureDeath = new TimeStampedDescription("DeathDate", dateDeath);
-			// add deathdate
-			person.setDeathDate(dateStructureDeath);
-			// IsPresent
-			assertTrue(person.getDeathDate().isPresent());
+		// Init DateStructure
+		TimeStampedDescription dateStructureBirth = new TimeStampedDescription("BirthDate", dateBirth);
+		TimeStampedDescription dateStructureDeath = new TimeStampedDescription("DeathDate", dateDeath);
 
-		} catch (NullPointerException | ParseException e) {
+		Person person = new Person("Shakespeare, William, 1564-1616", "William", "Shakespeare", dateStructureBirth);
 
-			e.printStackTrace();
-			assertTrue(true);
-		}
+		assertFalse(person.getDeathDate().isPresent());
+
+		// add deathdate
+		person.setDeathDate(dateStructureDeath);
+		// IsPresent
+		assertTrue(person.getDeathDate().isPresent());
 
 	}
 

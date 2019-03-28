332e3f6dcc1c81531c61515d38851f68ea6e04af
Vima Haddad <vima.haddad@gmail.com>
2019-03-28 16:26:52 +0100

    Item Servlet JAX-RS

--- src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/servlets/ItemJsonServlet.java
+++ src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/servlets/ItemJsonServlet.java
@@ -1,106 +1,57 @@
 package io.github.oliviercailloux.y2018.jbiblio.j_biblio.servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Locale;
+import java.util.List;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.MediaType;
-//import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import io.github.oliviercailloux.y2018.jbiblio.j_biblio.basicentities.Item;
-//import io.github.oliviercailloux.y2018.jbiblio.j_biblio.services.ItemService;
+import io.github.oliviercailloux.y2018.jbiblio.j_biblio.services.ItemService;
 
-@SuppressWarnings("serial")
-@WebServlet("item")
+@RequestScoped
+@Path("item")
 public class ItemJsonServlet extends HttpServlet {
+	@SuppressWarnings("unused")
 	private static final Logger LOGGER = Logger.getLogger(ItemJsonServlet.class.getCanonicalName());
+
 	@PersistenceContext
 	private EntityManager em;
 
-//	@Inject
-//	private ItemService itemService;
+	@Inject
+	private HttpServletRequest request;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	@Inject
+	private ItemService itemService;
 
-		resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-		resp.setContentType(MediaType.APPLICATION_JSON);
-		resp.setLocale(Locale.ENGLISH);
-		/**
-		 * Converting to Json format
-		 */
-		try (Jsonb jsonb = JsonbBuilder.create();) {
+	@GET
+	@Transactional
+	@Produces("application/json")
+	public List<Item> getItemsServlet() {
 
-			/**
-			 * Jpa will be implemented in the next sprint
-			 */
-			Item item = this.initItem(req);
-			String jsonItem = jsonb.toJson(item);
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.getWriter().println(jsonItem);
-			LOGGER.info(" Display object in JSON format " + jsonItem);
-
-		} catch (Exception e) {
-			// Send error status
-			LOGGER.warning(" Error  " + e.toString());
-			throw new ServletException(e);
-
-		}
+		List<Item> items = itemService.getAll();
+		return items;
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-		resp.setContentType(MediaType.TEXT_PLAIN);
-		resp.setLocale(Locale.ENGLISH);
-		try (Jsonb jsonb = JsonbBuilder.create();) {
-			try (BufferedReader reader = req.getReader()) {
-				/**
-				 * Jpa will be implemented in the next sprint
-				 */
-				jsonb.fromJson(reader, Item.class);
-
-			}
-			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.getWriter().println(
-					"The object is successfully initialized. Database insertion  will be implemented in the next sprint");
-		}
-
-		catch (Exception e) {
-
-			LOGGER.warning(" Error  " + e.toString());
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "insert failed.");
-		}
-
+	@POST
+	@Transactional
+	@Consumes("application/json")
+	public Response addItemServlet(Item item) throws IOException {
+		em.persist(item);
+		LOGGER.info("Item added!");
+		return Response.status(Response.Status.OK).build();
 	}
 
-	public Item initItem(HttpServletRequest req) throws NumberFormatException, NullPointerException {
-
-		int idItem = Integer.parseInt(req.getParameter("idItem"));
-		int idManifestation = Integer.parseInt(req.getParameter("idManifestation"));
-		/**
-		 * If parsInt doesn't throw NumberFormatException, then we can init a Item
-		 */
-		Item item = new Item();
-		item.setIdItem(idItem);
-		item.setIdManifestation(idManifestation);
-		// itemIdentifier must not be null
-		item.setItemIdentifier(req.getParameter("itemIdentifier"));
-		// if null will be converted to an empty string
-		item.setFingerprint(req.getParameter("fingerprint"));
-		item.setProvenanceOfTheItem(req.getParameter("provenanceOfTheItem"));
-
-		return item;
-
-	}
 }

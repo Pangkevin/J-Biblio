b7166a5110dd1007962ee57275d2723027758332
Pangkevin <kevin94310@hotmail.fr>
2019-03-29 20:30:30 +0100

    correction

--- src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/MyJaxRsApp.java
+++ src/main/java/io/github/oliviercailloux/y2018/jbiblio/j_biblio/services/MyJaxRsApp.java
@@ -3,7 +3,7 @@ package io.github.oliviercailloux.y2018.jbiblio.j_biblio.services;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("jbiblio")
+@ApplicationPath("/")
 public class MyJaxRsApp extends Application {
 	/** Empty. The server will then discover all resource classes automatically. */
 
